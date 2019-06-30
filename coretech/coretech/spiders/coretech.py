e6137793435f747d4de18bda4e8bf7372672c473
Sooyoung Moon <msy0128@gmail.com>
2019-07-01 06:31:55 +0900

    Update coretech.py

--- coretech/coretech/spiders/coretech.py
+++ coretech/coretech/spiders/coretech.py
@@ -1,6 +1,6 @@
 import scrapy
 
-URL = "https://coretech21c.co.kr/business/{navi}.jsp"
+URL = "http://coretech21c.co.kr/business/{navi}.jsp"
 navilist = ["catalyst", "environment", "gas"]
 
 class CoretechSpider(scrapy.Spider):
@@ -8,11 +8,10 @@ class CoretechSpider(scrapy.Spider):
     start_urls = [URL.format(navi=navi) for navi in navilist]
 
     def parse(self, response):
-        for item in response.css('div.catalyst_pop'):
-            yield {
-                'name': item.css('p::text').get(),
-            }
-            for category in item.css('li'):
+        for item in response.css("div.catalyst_pop"):
+            for category in item.css("li"):
                 yield {
-                    category.css('p::text').get() : category.css("span::text").getall()
+                    "name": item.css("p::text").get(),
+                    category.css("p::text").get() : \
+                        category.css("span::text").getall()
                 }
